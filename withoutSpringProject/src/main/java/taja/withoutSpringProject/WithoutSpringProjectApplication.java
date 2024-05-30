e7b0279b2aadc3a81f245eb3cf9645120559bf9e
noorykwon <noorykwon@iteyes.co.kr>
2024-05-30 21:03:08 +0900

    section 4 스프링 컨테이너와 스프링 빈

--- withoutSpringProject/src/main/java/taja/withoutSpringProject/WithoutSpringProjectApplication.java
+++ withoutSpringProject/src/main/java/taja/withoutSpringProject/WithoutSpringProjectApplication.java
@@ -3,7 +3,6 @@ package taja.withoutSpringProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class WithoutSpringProjectApplication {
 
 	public static void main(String[] args) {
