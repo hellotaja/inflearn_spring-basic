d7cba81adccb10504b65a375f86fcd4af0332485
taja <noorykwon@gmail.com>
2024-06-18 02:55:32 +0900

    section 5 싱글톤 컨테이너

---
+++ withoutSpringProject/src/test/java/taja/withoutSpringProject/xml/xmlAppContext.java
@@ -0,0 +1,19 @@
+package taja.withoutSpringProject.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import taja.withoutSpringProject.member.MemberService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class xmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
