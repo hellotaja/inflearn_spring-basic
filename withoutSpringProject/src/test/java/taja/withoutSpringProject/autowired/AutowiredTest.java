b0eac74813efe8ac98eee9a01f773ddd93377069
noorykwon <noorykwon@iteyes.co.kr>
2024-08-08 21:04:29 +0900

    section 7 의존관계 자동 주입

--- withoutSpringProject/src/test/java/taja/withoutSpringProject/autowired/AutowiredTest.java
+++ withoutSpringProject/src/test/java/taja/withoutSpringProject/autowired/AutowiredTest.java
@@ -1,11 +1,20 @@
 package taja.withoutSpringProject.autowired;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutowiredTest {
 
     @Test
     void AutowiredOption () {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+
 
     }
+
+    static class TestBean {
+
+    }
+
 }
